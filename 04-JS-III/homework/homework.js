6c34e430ca418736e449e79c1767f0f7dafaec1b
Dr3sden <jossotosoutherland@gmail.com>
2022-06-15 16:07:57 -0400

    a

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -156,7 +156,7 @@ function diaDeLaSemana(numeroDeDia) {
     return "Es fin de semana";
   }
   
-  return "Es dia laboral";
+  return "Es dia Laboral";
 }
 
 
@@ -196,12 +196,17 @@ function mesesDelAño(array) {
       meses.push(array[i]);
     }
   }
-  for(var i=0; i<meses.length; i++){
+  /*for(var i=0; i<meses.length; i++){
     if(meses[i]!= "Enero" || meses[i]!= "Marzo" || meses[i]!= "Noviembre" ){
       return "No se econtraron los meses pedidos"
     }
+  }*/
+  if(meses.length<3){
+    return "No se econtraron los meses pedidos"
   }
+  else{
   return meses;
+  }
 }
 
 
