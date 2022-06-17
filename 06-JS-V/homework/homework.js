31cab8b8c5ba2fb61929cf1ba85fc809d937e155
Dr3sden <jossotosoutherland@gmail.com>
2022-06-17 15:20:36 -0400

    a

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -43,7 +43,7 @@ function agregarStringInvertida() {
       invertida = invertida + this.charAt[i];
     }
     return invertida;
-  }
+  };
 
 }
 
@@ -80,6 +80,8 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
+  var persona = new Persona(nombre, apellido, Perez, edad, dir);
+  return persona;
 }
   
 function agregarMetodo() {
