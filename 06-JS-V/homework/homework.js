e2ae089069f83414767fa369b4235567f074718a
Dr3sden <jossotosoutherland@gmail.com>
2022-06-17 15:30:51 -0400

    a

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -37,14 +37,13 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
-  String.prototype.reverse = function(){
+  String.prototype.reverse = function(String){
     var invertida = "";
     for(var i= this.length-1; i>=0; i--){
       invertida = invertida + this.charAt[i];
     }
     return invertida;
   };
-
 }
 
 // ---------------------------------------------------------------------------//
