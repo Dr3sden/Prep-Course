fb17301b6ca7eebb2d03b672b2018dbbb3188f34
Dr3sden <jossotosoutherland@gmail.com>
2022-06-29 09:50:33 -0400

    a

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -109,8 +109,8 @@ function sortArray(arr) {
   var arraux = [];
   for(var i=0; i<arr.length; i++){
     for(var j=0; j<arr.length; j++){
-      if(arr[i].length<=arr[j].length){
-        arraux.unshift(arr[i]);
+      if(arr[i].length<arr[j].length){
+        arraux.push(arr[i]);
       }
     }
   }
